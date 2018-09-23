2bf21d74c08d656ac28fba695f212b4a15f1bde2
Jonathan Silva <contato@condeweb.com.br>
2018-09-23 17:08:50 -0300

    Testes

--- app/src/main/java/app/mvp/helper/ToastHelper.java
+++ app/src/main/java/app/mvp/helper/ToastHelper.java
@@ -3,8 +3,15 @@ package app.mvp.helper;
 import android.app.Activity;
 import android.widget.Toast;
 
+import app.mvp.model.User;
+import retrofit2.Response;
+
 public class ToastHelper {
     public static void alert(String message, Activity activity) {
         Toast.makeText(activity, message, Toast.LENGTH_LONG).show();
+    }
+
+    public static boolean isValidaResponse(User resp, Response<User> response) {
+        return resp != null && response.isSuccessful() && response.body() != null;
     }
 }
--- app/src/main/java/app/mvp/ui/fragment/login/PasswordLoginPresenter.java
+++ app/src/main/java/app/mvp/ui/fragment/login/PasswordLoginPresenter.java
@@ -4,6 +4,7 @@ import android.support.annotation.NonNull;
 
 import java.io.IOException;
 
+import app.mvp.helper.ToastHelper;
 import app.mvp.helper.ValidatorHelper;
 import app.mvp.model.User;
 import app.mvp.retrofit.Config;
@@ -36,13 +37,9 @@ public class PasswordLoginPresenter implements PasswordLoginContract.PasswordLog
                 public void onResponse(@NonNull Call<User> call, @NonNull retrofit2.Response<User> response) {
                     final User resp = response.body();
 
-                    if (resp != null) {
-                        if (response.isSuccessful()) {
-                            if (response.body() != null) {
-                                session.setLogin(resp);
-                                view.openDashboard();
-                            }
-                        }
+                    if (ToastHelper.isValidaResponse(resp, response)) {
+                        session.setLogin(resp);
+                        view.openDashboard();
                     } else {
                         view.errorLogin();
                     }
